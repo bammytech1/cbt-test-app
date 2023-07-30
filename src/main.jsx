dbfc12e6bb90ca7ed27b709212e7932d9d3deb23
bammytech1 <66552185+bammytech1@users.noreply.github.com>
2023-07-30 23:43:32 +0100

    redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+// import { BrowserRouter } from "react-router-dom"
+import store from "./redux/store.jsx";
+import { Provider } from "react-redux";
 
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from "react-router-dom"
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-</BrowserRouter>
-)
+  </Provider>
+);
